e73e242eeb11cbe76e3ec75658f19d8b661faa60
fabi <fabi@adres.pl>
2017-06-24 15:56:25 +0200

    My Django Girls app, first commit

---
+++ imie.py
@@ -0,0 +1,13 @@
+imie = "licz" #komentarz-save!
+print("Czesc! Jak masz na imie?")
+input_var = input(" ")
+print ("Czesc " + input_var)
+#print("To teraz czas na liczenie!")
+#def zadanie1():
+#    if imie == 'dodawanie':
+#        print("podaj 1wsza cyfre")
+#    if imie == 'odejmowanie':
+#        print("podaj 1wsza cyfre")
+#    else:
+#        print("wpisz: dodawanie lub odejmowanie")
+#def()
